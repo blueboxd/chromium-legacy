d88dbf58e00ea8741ce04d842e27a8f0d9d49fe4
bluebox <bluebox@b5x.jp>
2021-02-08 05:12:15 +0900

    Revert "merge d9ff213a3c9cce8c01fc2ee29c17e1e06db1d8a1 and 23ddbaa2c4c91a0613f4986a925c94a56bb5228b manually"
    This reverts commit 2c2f67cb126b1a72874fb22c1424062e31bdfce2.
    23ddbaa2c4c91a0613f4986a925c94a56bb5228b was reverted before due to 'atomic store requires runtime support that is not available for this target'

--- base/threading/threading_features.h
+++ base/threading/threading_features.h
@@ -14,12 +14,6 @@ struct Feature;
 
 #if defined(OS_APPLE)
 extern const BASE_EXPORT Feature kOptimizedRealtimeThreadingMac;
-extern const BASE_EXPORT FeatureParam<bool>
-    kOptimizedRealtimeThreadingMacPreemptible;
-extern const BASE_EXPORT FeatureParam<double>
-    kOptimizedRealtimeThreadingMacBusy;
-extern const BASE_EXPORT FeatureParam<double>
-    kOptimizedRealtimeThreadingMacBusyLimit;
 #endif
 
 }  // namespace base

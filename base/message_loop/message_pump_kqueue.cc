a2debb252261d27c6bef939e9221f573222af626
blueboxd <48254131+blueboxd@users.noreply.github.com>
2022-06-08 13:14:24 +0900

    Revert "Remove OS X 10.11 and macOS 10.12 code from base::MessagePumpKqueue"
    This reverts commit 2c8778f9466732a82db69daf9d80326aa59a3c68.
    
    # Conflicts:
    #	base/message_loop/message_pump_kqueue.cc

--- base/message_loop/message_pump_kqueue.cc
+++ base/message_loop/message_pump_kqueue.cc
@@ -114,15 +114,31 @@ MessagePumpKqueue::MessagePumpKqueue()
       base::mac::ScopedMachReceiveRight::Receiver(wakeup_).get());
   MACH_CHECK(kr == KERN_SUCCESS, kr) << "mach_port_allocate";
 
-  // Configure the event to directly receive the Mach message as part of the
-  // kevent64() call.
   kevent64_s event{};
-  event.ident = wakeup_.get();
-  event.filter = EVFILT_MACHPORT;
-  event.flags = EV_ADD;
-  event.fflags = MACH_RCV_MSG;
-  event.ext[0] = reinterpret_cast<uint64_t>(&wakeup_buffer_);
-  event.ext[1] = sizeof(wakeup_buffer_);
+  if (KqueueNeedsPortSet()) {
+    kr = mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_PORT_SET,
+                            mac::ScopedMachPortSet::Receiver(port_set_).get());
+    MACH_CHECK(kr == KERN_SUCCESS, kr) << "mach_port_allocate PORT_SET";
+
+    kr = mach_port_insert_member(mach_task_self(), wakeup_.get(),
+                                 port_set_.get());
+    MACH_CHECK(kr == KERN_SUCCESS, kr) << "mach_port_insert_member";
+
+    event.ident = port_set_.get();
+    event.filter = EVFILT_MACHPORT;
+    event.flags = EV_ADD;
+  } else {
+    // When not using a port set, the wakeup port event can be specified to
+    // directly receive the Mach message as part of the kevent64() syscall.
+    // This is not done when using a port set, since that would potentially
+    // receive client MachPortWatchers' messages.
+    event.ident = wakeup_.get();
+    event.filter = EVFILT_MACHPORT;
+    event.flags = EV_ADD;
+    event.fflags = MACH_RCV_MSG;
+    event.ext[0] = reinterpret_cast<uint64_t>(&wakeup_buffer_);
+    event.ext[1] = sizeof(wakeup_buffer_);
+  }
 
   int rv = ChangeOneEvent(kqueue_, &event);
   PCHECK(rv == 0) << "kevent64";
@@ -202,16 +218,25 @@ bool MessagePumpKqueue::WatchMachReceivePort(
     return false;
   }
 
-  kevent64_s event{};
-  event.ident = port;
-  event.filter = EVFILT_MACHPORT;
-  event.flags = EV_ADD;
-  int rv = ChangeOneEvent(kqueue_, &event);
-  if (rv < 0) {
-    DPLOG(ERROR) << "kevent64";
-    return false;
+  if (KqueueNeedsPortSet()) {
+    kern_return_t kr =
+        mach_port_insert_member(mach_task_self(), port, port_set_.get());
+    if (kr != KERN_SUCCESS) {
+      MACH_LOG(ERROR, kr) << "mach_port_insert_member";
+      return false;
+    }
+  } else {
+    kevent64_s event{};
+    event.ident = port;
+    event.filter = EVFILT_MACHPORT;
+    event.flags = EV_ADD;
+    int rv = ChangeOneEvent(kqueue_, &event);
+    if (rv < 0) {
+      DPLOG(ERROR) << "kevent64";
+      return false;
+    }
+    ++event_count_;
   }
-  ++event_count_;
 
   controller->Init(weak_factory_.GetWeakPtr(), port, delegate);
   port_controllers_.AddWithID(controller, port);
@@ -297,15 +322,24 @@ bool MessagePumpKqueue::StopWatchingMachPort(
   controller->Reset();
   port_controllers_.Remove(port);
 
-  kevent64_s event{};
-  event.ident = port;
-  event.filter = EVFILT_MACHPORT;
-  event.flags = EV_DELETE;
-  --event_count_;
-  int rv = ChangeOneEvent(kqueue_, &event);
-  if (rv < 0) {
-    DPLOG(ERROR) << "kevent64";
-    return false;
+  if (KqueueNeedsPortSet()) {
+    kern_return_t kr =
+        mach_port_extract_member(mach_task_self(), port, port_set_.get());
+    if (kr != KERN_SUCCESS) {
+      MACH_LOG(ERROR, kr) << "mach_port_extract_member";
+      return false;
+    }
+  } else {
+    kevent64_s event{};
+    event.ident = port;
+    event.filter = EVFILT_MACHPORT;
+    event.flags = EV_DELETE;
+    --event_count_;
+    int rv = ChangeOneEvent(kqueue_, &event);
+    if (rv < 0) {
+      DPLOG(ERROR) << "kevent64";
+      return false;
+    }
   }
 
   return true;
@@ -422,10 +456,21 @@ bool MessagePumpKqueue::ProcessEvents(Delegate* delegate, int count) {
         fd_watcher->OnFileCanWriteWithoutBlocking(event->ident);
       }
     } else if (event->filter == EVFILT_MACHPORT) {
-      mach_port_t port = event->ident;
+      mach_port_t port = KqueueNeedsPortSet() ? event->data : event->ident;
+
       if (port == wakeup_.get()) {
         // The wakeup event has been received, do not treat this as "doing
         // work", this just wakes up the pump.
+        if (KqueueNeedsPortSet()) {
+          // When using the kqueue directly, the message can be received
+          // straight into a buffer that was created when adding the event.
+          // But when using a port set, the message must be drained manually.
+          wakeup_buffer_.header.msgh_local_port = port;
+          wakeup_buffer_.header.msgh_size = sizeof(wakeup_buffer_);
+          kern_return_t kr = mach_msg_receive(&wakeup_buffer_.header);
+          MACH_LOG_IF(ERROR, kr != KERN_SUCCESS, kr)
+              << "mach_msg_receive wakeup";
+        }
         continue;
       }
 
